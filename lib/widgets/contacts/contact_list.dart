87ec0cd791b5a387dfae6a263bed437a4fafa296
ShapeShifterHades <sotnichenko1019@gmail.com>
2020-12-04 14:10:29 +0300

    1st commit of the day 04.12.2020 2-10PM

--- lib/widgets/contacts/contact_list.dart
+++ lib/widgets/contacts/contact_list.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:void_chat_beta/widgets/contacts/remove_from_contacts_button.dart';
 
-class ContactList extends StatefulWidget {
+class ContactList extends StatelessWidget {
   final String myId;
   final String myUsername;
 
@@ -15,181 +14,125 @@ class ContactList extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _ContactListState createState() => _ContactListState();
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          Expanded(
+            child: GetMyContactList(
+              myId: myId,
+              myUsername: myUsername,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _ContactListState extends State<ContactList> {
-  Future<User> getName() async {
-    return FirebaseAuth.instance.currentUser;
-  }
+class GetMyContactList extends StatelessWidget {
+  final String myId;
+  final String myUsername;
+
+  const GetMyContactList({Key key, this.myId, this.myUsername})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.lightGreen,
-        child: FutureBuilder(
-            future: getName(),
-            builder: (ctx, futureSnapshot) {
-              if (futureSnapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+    return Container(
+      color: Colors.lightGreen,
+      child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(myId)
+              .collection('contacts')
+              .snapshots(),
+          builder: (ctx, contactsSnapshot) {
+            if (contactsSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (!(contactsSnapshot.data.documents.length > 0)) {
+              return Center(
+                child: Text('No contacts yet'),
+              );
+            }
+            final listItem = contactsSnapshot.data.documents;
+            return ListView.builder(
+                itemCount: listItem.length,
+                itemBuilder: (ctx, index) {
+                  checkIt() async {
+                    var doc = await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(myId)
+                        .collection('addedBy')
+                        .doc(listItem[index].documentID)
+                        .get();
+                    return doc.exists;
+                  }
 
-              return StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(futureSnapshot.data.uid)
-                      .collection('contacts')
-                      .snapshots(),
-                  builder: (ctx, contactsSnapshot) {
-                    if (contactsSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (!(contactsSnapshot.data.documents.length > 0)) {
-                      return Center(
-                        child: Text('No contacts yet'),
-                      );
-                    }
-                    final listItem = contactsSnapshot.data.documents;
-                    return ListView.builder(
-                        itemCount: listItem.length,
-                        itemBuilder: (ctx, index) {
-                          checkIt() async {
-                            var doc = await FirebaseFirestore.instance
-                                .collection('users')
-                                .doc(widget.myId)
-                                .collection('addedBy')
-                                .doc(listItem[index].documentID)
-                                .get();
-                            return doc.exists;
-                          }
+                  return FutureBuilder(
+                      future: checkIt(),
+                      builder: (ctx, snapShot) {
+                        if (snapShot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
 
-                          return FutureBuilder(
-                              future: checkIt(),
-                              builder: (ctx, snapShot) {
-                                if (snapShot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                }
-                                if (snapShot.data) {
-                                  print(snapShot.data);
-                                  return Container(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Container(
-                                        padding: EdgeInsets.all(12),
-                                        color: Colors.white,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceEvenly,
-                                          children: [
-                                            Container(
-                                              child: Expanded(
-                                                flex: 1,
-                                                child: ListTile(
-                                                  key: Key(listItem[index]
-                                                      .documentID),
-                                                  title: Text(
-                                                    listItem[index]['username'],
-                                                  ),
-                                                  subtitle:
-                                                      Text('In contact list'),
-                                                  onTap: () {},
-                                                ),
-                                              ),
-                                            ),
-                                            Expanded(
-                                              flex: 1,
-                                              child: RemoveFromContactsButton(
-                                                myId: widget.myId,
-                                                myUsername: widget.myUsername,
-                                                userId:
-                                                    listItem[index].documentID,
-                                                username: listItem[index]
-                                                    ['username'],
-                                              ),
-                                            ),
-                                            Expanded(
-                                              flex: 1,
-                                              child: MaterialButton(
-                                                padding: EdgeInsets.all(6),
-                                                color: Colors.blue,
-                                                onPressed: () {},
-                                                child: Text('Message'),
-                                              ),
-                                            ),
-                                          ],
+                        return Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              padding: EdgeInsets.all(12),
+                              color: Colors.white,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                    child: Expanded(
+                                      flex: 1,
+                                      child: ListTile(
+                                        key: Key(listItem[index].documentID),
+                                        title: Text(
+                                          listItem[index]['username'],
                                         ),
+                                        subtitle: snapShot.data
+                                            ? Text('In contact list')
+                                            : Text('Not in contact list'),
+                                        onTap: () {},
                                       ),
                                     ),
-                                  );
-                                } else {
-                                  return Container(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Container(
-                                        padding: EdgeInsets.all(12),
-                                        color: Colors.white,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceEvenly,
-                                          children: [
-                                            Container(
-                                              child: Expanded(
-                                                flex: 1,
-                                                child: ListTile(
-                                                  key: Key(listItem[index]
-                                                      .documentID),
-                                                  title: Text(
-                                                    listItem[index]['username'],
-                                                  ),
-                                                  subtitle: Text(
-                                                      'U are not in persons contact'),
-                                                  onTap: () {},
-                                                ),
-                                              ),
-                                            ),
-                                            Expanded(
-                                              flex: 1,
-                                              child: RemoveFromContactsButton(
-                                                myId: widget.myId,
-                                                myUsername: widget.myUsername,
-                                                userId:
-                                                    listItem[index].documentID,
-                                                username: listItem[index]
-                                                    ['username'],
-                                              ),
-                                            ),
-                                            Expanded(
-                                              flex: 1,
-                                              child: MaterialButton(
-                                                padding: EdgeInsets.all(6),
-                                                color: Colors.blue,
-                                                disabledColor: Colors.blueGrey,
-                                                disabledTextColor:
-                                                    Colors.white60,
-                                                onPressed: null,
-                                                child: Text('Message'),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: RemoveFromContactsButton(
+                                      myId: myId,
+                                      myUsername: myUsername,
+                                      userId: listItem[index].documentID,
+                                      username: listItem[index]['username'],
                                     ),
-                                  );
-                                }
-                              });
-                        });
-                  });
-            }),
-      ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: MaterialButton(
+                                      padding: EdgeInsets.all(6),
+                                      color: Colors.blue,
+                                      onPressed: snapShot.data ? () {} : null,
+                                      child: Text('Message'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      });
+                });
+          }),
     );
   }
 }
