d32799f5c68608f08ff5c0d87743a5d600da627f
ShapeShifterHades <sotnichenko1019@gmail.com>
2021-05-18 00:36:26 +0300

    bubble bg

--- lib/presentation/screens/chat_screen/widgets/message_bubble/bubble.dart
+++ lib/presentation/screens/chat_screen/widgets/message_bubble/bubble.dart
@@ -151,3 +151,44 @@ class _MessageBubbleState extends State<MessageBubble>
     );
   }
 }
+
+@immutable
+class BubbleBackground extends StatelessWidget {
+  const BubbleBackground({
+    Key? key,
+    required this.colors,
+    this.child,
+  }) : super(key: key);
+
+  final List<Color> colors;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: BubblePainter(
+        colors: colors,
+      ),
+      child: child,
+    );
+  }
+}
+
+class BubblePainter extends CustomPainter {
+  BubblePainter({
+    required List<Color> colors,
+  }) : _colors = colors;
+
+  final List<Color> _colors;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO:
+  }
+
+  @override
+  bool shouldRepaint(BubblePainter oldDelegate) {
+    // TODO:
+    return false;
+  }
+}
