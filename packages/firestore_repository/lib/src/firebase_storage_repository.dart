94e15dadfce430896beef3f6ee534b4d97496b06
ShapeShifterHades <sotnichenko1019@gmail.com>
2021-06-04 11:38:15 +0300

    focusIndex

--- packages/firestore_repository/lib/src/firebase_storage_repository.dart
+++ packages/firestore_repository/lib/src/firebase_storage_repository.dart
@@ -1,11 +1,14 @@
-// import 'dart:io';
-// import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:flutter/material.dart';
 
-// class FirebaseStorageRepository {
-//   firebase_storage.FirebaseStorage storage =
-//       firebase_storage.FirebaseStorage.instance;
+class FirebaseStorageRepository {
+  firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
 
-//   uploadImage(String uid, File file) async {
-//     //  var storageRef = storage.ref().child(path)
-//   }
-// }
+  uploadImage(String uid, File file) async {
+    //  var storageRef = storage.ref().child(path)
+  }
+}
+
+class Nigger extends ChangeNotifier {}
